c3ff53e1a04c9d5968ee4312d995d85295c63aef
Albert Kulakowski <9albert6@gmail.com>
2019-12-05 09:41:21 +0100

    Changes test to not working.

--- src/test/java/edu/iis/mto/lab_0/TailsTest.java
+++ src/test/java/edu/iis/mto/lab_0/TailsTest.java
@@ -24,7 +24,7 @@ public class TailsTest {
     @Test
     public void tailsShouldReturnListOfStringsOfSizeEqualsToInputStringLengthPlusOne() {
 
-        assertThat(tails, Matchers.hasSize(HELLO.length() + 1));
+        assertThat(tails, Matchers.hasSize(HELLO.length()));
     }
 
     @Test
